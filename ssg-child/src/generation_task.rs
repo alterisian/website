f8f2272e3a4f0587e63006c03590e9e98914ad83
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-06-13 13:27:28 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:ssg-child/src/lib.rs

--- ssg-child/src/generation_task.rs
+++ ssg-child/src/generation_task.rs
@@ -1,1 +1,25 @@
-pub struct GenerationTask;
+use std::future::IntoFuture;
+
+use futures::{Stream, StreamExt};
+
+use crate::{target_error::TargetError, target_success::TargetSuccess, final_error::FinalError};
+
+pub struct GenerationTask(Box<dyn Stream<Item = Result<TargetSuccess, TargetError>>>);
+
+impl GenerationTask {
+    pub(crate) fn new(
+        stream: impl Stream<Item = Result<TargetSuccess, TargetError>> + 'static,
+    ) -> Self {
+        Self(Box::new(stream))
+    }
+}
+
+impl IntoFuture for GenerationTask {
+    type Output = Result<(), FinalError>;
+
+    type IntoFuture = ();
+
+    fn into_future(self) -> Self::IntoFuture {
+        self.0.fold(FinalErrorBuilder, |)
+    }
+}
--- ssg-child/src/lib.rs
+++ ssg-child/src/lib.rs
@@ -8,19 +8,20 @@ pub mod target_success;
 
 use camino::Utf8PathBuf;
 pub use file_spec::FileSpec;
-use futures::{stream::{self, BufferUnordered}, StreamExt};
+use futures::{
+    stream::{self, BufferUnordered},
+    StreamExt,
+};
 use generation_task::GenerationTask;
 
 use target_error::TargetError;
 
-type Stream = ();
-
 /// Panics on duplicate `FileSpec` targets
 pub fn generate_static_site(
     output_dir: Utf8PathBuf,
-    file_specs: impl IntoIterator<Item = FileSpec>,
+    file_specs: impl IntoIterator<Item = FileSpec> + 'static,
 ) -> GenerationTask {
-    let stream: Stream = stream::iter(file_specs)
+    let stream = stream::iter(file_specs)
         .map(move |file_spec| file_spec.generate(output_dir.clone()))
         .buffer_unordered(usize::MAX);
 
